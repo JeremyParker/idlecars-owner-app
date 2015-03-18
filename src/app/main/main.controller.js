35689ac52708dbe7c3ef577a0e6052d45f4e2b48
jefk <speedracer111@gmail.com>
2015-03-17 21:05:34 -0400

    test response from local server

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,6 +1,8 @@
 'use strict';
 
 angular.module('idlecars')
-.controller('MainCtrl', function ($scope) {
-  $scope.greeting = "Hello, carless drivers!"
+.controller('MainCtrl', function ($scope, $http) {
+  $http.get('http://localhost:8000/').then(function(response) {
+    $scope.cars = response.data;
+  })
 });
