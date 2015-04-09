7f8751fd7cdd64d972487bc33b52da3ef727e0e4
jefk <speedracer111@gmail.com>
2015-04-09 12:32:11 -0400

    remove console log

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,5 @@
 'use strict';
-console.log('index.js')
+
 angular.module('idlecars', ['ngAnimate', 'ngCookies', 'ngSanitize', 'ui.router', 'ngMaterial', 'ic.appConfig'])
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
