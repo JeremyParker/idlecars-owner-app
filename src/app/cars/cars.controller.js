e02e178ba40c5eb304dfb95dd869f8019613dc69
jefk <speedracer111@gmail.com>
2015-04-12 15:58:39 -0400

    show some cars

--- src/app/cars/cars.controller.js
+++ src/app/cars/cars.controller.js
@@ -1,6 +1,9 @@
 'use strict';
 
 angular.module('idlecars')
-.controller('CarsCtrl', function ($scope, $http, config) {
-  $scope.baseUrl = config.api_base_url;
+.controller('CarsCtrl', function ($scope, $http) {
+  $scope.cars = [
+    {make: 'Lincoln', model: 'MKT', year: '2013'},
+    {make: 'GMC', model: 'Yukon', year: '2014'},
+  ];
 });
