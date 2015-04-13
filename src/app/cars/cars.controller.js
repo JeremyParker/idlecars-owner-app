2fd4e6a5c3cd8c8f5b171fc0c64a57f0209c3f37
jefk <speedracer111@gmail.com>
2015-04-12 22:36:39 -0400

    redirect to car detail

--- src/app/cars/cars.controller.js
+++ src/app/cars/cars.controller.js
@@ -3,7 +3,7 @@
 angular.module('idlecars')
 .controller('CarsCtrl', function ($scope, $http) {
   $scope.cars = [
-    {make: 'Lincoln', model: 'MKT', year: '2013'},
-    {make: 'GMC', model: 'Yukon', year: '2014'},
+    {id: 5, make: 'Lincoln', model: 'MKT', year: '2013'},
+    {id: 23, make: 'GMC', model: 'Yukon', year: '2014'},
   ];
 });
--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,24 @@
 'use strict';
 
 angular.module('idlecars', ['ngAnimate', 'ngCookies', 'ui.router', 'ic.appConfig'])
-  .config(function ($stateProvider, $urlRouterProvider) {
+  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
     $stateProvider
+
       .state('carIndex', {
         url: '/',
-        templateUrl: 'app/cars/cars.html',
+        templateUrl: 'app/cars/index.html',
         controller: 'CarsCtrl'
+      })
+
+      .state('carDetail', {
+        url: 'car/:carId',
+        templateUrl: 'app/cars/show.html',
+        controller: function ($scope, $stateParams) {
+          $scope.carId = $stateParams.carId;
+        }
       });
 
     $urlRouterProvider.otherwise('/');
+    $locationProvider.html5Mode(true);
   })
 ;
