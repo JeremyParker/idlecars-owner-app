48737792a9400089f9031f4c1fda4c607732a191
jefk <speedracer111@gmail.com>
2015-04-12 23:03:55 -0400

    disable html5 mode

--- src/app/index.js
+++ src/app/index.js
@@ -1,25 +1,24 @@
 'use strict';
 
 angular.module('idlecars', ['ngAnimate', 'ngCookies', 'ui.router', 'ic.appConfig'])
-  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
-    $stateProvider
+.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+  $stateProvider
 
-      .state('carIndex', {
-        url: '/',
-        templateUrl: 'app/cars/index.html',
-        controller: 'CarsCtrl'
-      })
+    .state('carIndex', {
+      url: '/',
+      templateUrl: 'app/cars/index.html',
+      controller: 'CarsCtrl'
+    })
 
-      .state('carDetail', {
-        url: 'car/:carId',
-        templateUrl: 'app/cars/show.html',
-        params: {car: null},
-        controller: function ($scope, $stateParams) {
-          $scope.car = $stateParams.car;
-        }
-      });
+    .state('carDetail', {
+      url: 'car/:carId',
+      templateUrl: 'app/cars/show.html',
+      params: {car: null},
+      controller: function ($scope, $stateParams) {
+        $scope.car = $stateParams.car;
+      }
+    });
 
-    $urlRouterProvider.otherwise('/');
-    $locationProvider.html5Mode(true);
-  })
+  $urlRouterProvider.otherwise('/');
+})
 ;
