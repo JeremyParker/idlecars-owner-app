178d8d9518f669e544a69129cb210dbccb2ecf0d
jefk <speedracer111@gmail.com>
2015-04-20 16:09:08 -0400

    set response to car scope

--- src/app/cars/index.controller.js
+++ src/app/cars/index.controller.js
@@ -2,26 +2,7 @@
 
 angular.module('idlecars')
 .controller('cars.indexCtrl', function ($scope, $http, Car) {
-  $scope.cars = [
-    {
-      id: 3,
-      name: '2014 Toyota Camry',
-      listing_features: 'These Come From Server ∙ Brooklyn, NY ∙ Idlecars Certified',
-      cost: '425',
-      cost_time: 'a week',
-      subheads: ['Week-to-week ∙ Brooklyn, NY', '$425 Deposit Required'],
-    },
-    {
-      id: 5,
-      name: '2005 Chevrolet Suburban',
-      listing_features: 'Has a Funky Smell ∙ Queens, NY',
-      cost: '650',
-      cost_time: 'a week',
-      subheads: ['2 week minimum ∙ Queens, NY', '$1,000 Deposit Required'],
-    },
-  ];
-
   Car.query().$promise.then(function(cars) {
-    console.log(cars)
+    $scope.cars = cars;
   });
 });
