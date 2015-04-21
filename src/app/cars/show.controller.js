35b34261faf03941029606940395ba328ee029ba
jefk <speedracer111@gmail.com>
2015-04-21 11:29:42 -0400

    submit the booking to the server

--- src/app/cars/show.controller.js
+++ src/app/cars/show.controller.js
@@ -1,6 +1,19 @@
 'use strict';
 
 angular.module('idlecars')
-.controller('cars.showCtrl', function ($scope, $stateParams) {
+.controller('cars.showCtrl', function ($scope, $stateParams, BookingService) {
   $scope.car = $stateParams.car;
+
+  $scope.createBooking = function(accountParams) {
+    var bookingParams = {
+      user_account: accountParams,
+      car_id: $scope.car.id,
+    }
+    var newBooking = new BookingService(bookingParams);
+    newBooking.$save().then(_saveDidComplete);
+  }
+
+  var _saveDidComplete = function() {
+    console.log('this needs to be implemented');
+  }
 });
