31314cda268a8c5e18d4a03a892d01cd1d9c7462
craigstar <Man@MantekiMacBook-Pro.local>
2015-04-27 14:47:37 -0400

    included cookies of the browser

--- src/app/cars/index.controller.js
+++ src/app/cars/index.controller.js
@@ -1,8 +1,21 @@
 'use strict';
 
 angular.module('idlecars')
-.controller('cars.indexCtrl', function ($scope, $http, CarService) {
+.controller('cars.indexCtrl', function ($scope, $http, CarService, $cookieStore) {
   CarService.query().$promise.then(function(cars) {
     $scope.cars = cars;
   });
+
+  // check the browser cookies, show welcome text at first visit
+  var first_time = $cookieStore.get('first_time');
+
+  if (first_time == null) {
+    $scope.modal_show = true;
+    $cookieStore.put('first_time','no');
+  }
+  else {
+    $scope.modal_show = false;
+  }
 });
+
+
