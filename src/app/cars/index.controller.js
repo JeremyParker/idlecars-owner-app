f8fafccdd124a2c2be971079ee5c50a78248aa16
craigstar <Man@MantekiMacBook-Pro.local>
2015-04-28 17:41:41 -0400

    almost ready to go

--- src/app/cars/index.controller.js
+++ src/app/cars/index.controller.js
@@ -1,26 +1,31 @@
 'use strict';
 
 angular.module('idlecars')
-.controller('cars.indexCtrl', function ($scope, $http, CarService, $cookieStore) {
+.controller('cars.indexCtrl', function ($scope, $http, CarService, $cookieStore, $rootScope) {
   CarService.query().$promise.then(function(cars) {
     $scope.cars = cars;
   });
 
-  // colse button
-  $scope.closeModal = function() {
-    $scope.modal_show = false;
-  }
-
   // check the browser cookies, show welcome text at first visit
   var first_time = $cookieStore.get('first_time');
-
   if (first_time == null) {
-    $scope.modal_show = true;
+    $rootScope.modal_show = true;
     $cookieStore.put('first_time','no');
+    $rootScope.toDisable = true;
   }
   else {
-    $scope.modal_show = false;
+    $rootScope.toDisable = false;
+    $rootScope.modal_show = false;
   }
-});
+})
+
+.controller('modal.controller', function ($scope, $rootScope) {
+
+  // colse button clicked
+  $scope.closeModal = function() {
+    $scope.modal_show = false;
+    $rootScope.toDisable = false;
+  }
+})
 
 
---
+++ src/app/cars/modal.directive.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('idlecars')
+.directive('modal', function () {
+  return {
+    templateUrl: 'app/cars/modal.html',
+    controller: 'modal.controller',
+  }
+});
