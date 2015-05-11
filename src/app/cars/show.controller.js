2756c55c5246031a5a2819bc82677532023a1722
jefk <speedracer111@gmail.com>
2015-05-11 12:21:11 -0400

    go back to index if the car is not found

--- src/app/cars/show.controller.js
+++ src/app/cars/show.controller.js
@@ -3,9 +3,14 @@
 angular.module('idlecars')
 .controller('cars.showCtrl', function ($scope, $state, $stateParams, BookingService, CarService) {
   if (!$stateParams.car) {
-    CarService.get({carId: $stateParams.carId}).$promise.then(function(car) {
-      $scope.car = car;
-    });
+    CarService.get({carId: $stateParams.carId}).$promise.then(
+      function(car) {
+        $scope.car = car;
+      },
+      function(response) {
+        $state.go('cars');
+      }
+    );
   }
 
   $scope.car = $stateParams.car;
