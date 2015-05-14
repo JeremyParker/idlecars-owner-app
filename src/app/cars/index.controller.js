710259e62b5e8ac61effebfac3af1fb4e4f2d5f2
craigstar <Man@MantekiMacBook-Pro.local>
2015-05-14 11:10:12 -0400

    deleted redundant mapService

--- src/app/cars/index.controller.js
+++ src/app/cars/index.controller.js
@@ -1,12 +1,11 @@
 'use strict';
 
 angular.module('idlecars')
-.controller('cars.indexCtrl', function ($scope, $http, CarService, isRepeatVisitor, $rootScope, mapService) {
+.controller('cars.indexCtrl', function ($scope, $http, CarService, isRepeatVisitor, $rootScope) {
   CarService.query().$promise.then(function(cars) {
     $scope.cars = cars;
   });
 
   // check the browser cookies, show welcome text at first visit
   isRepeatVisitor.checkNow();
-
 });
