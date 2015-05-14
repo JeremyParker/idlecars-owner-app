3db1016219a732fc7cec876d32804285fbe32b2e
craigstar <Man@MantekiMacBook-Pro.local>
2015-05-14 16:20:01 -0400

    add a simple navbar

---
+++ src/components/navbar/nav_bar.directive.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('idlecars')
+.directive('navBar', function () {
+  return {
+    templateUrl: 'components/navbar/navbar.html',
+    controller: 'navbar.controller',
+    scope: {
+      back: "@"
+    }
+  };
+});
--- src/components/navbar/navbar.controller.js
+++ src/components/navbar/navbar.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
 angular.module('idlecars')
-  .controller('NavbarCtrl', function ($scope) {
-    $scope.date = new Date();
+  .controller('navbar.controller', function ($scope) {
+    console.log($scope.back)
   });
