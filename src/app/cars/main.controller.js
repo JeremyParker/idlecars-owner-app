9c45e500cec25778326d0bfffbae154b48c0575e
craigstar <Man@MantekiMacBook-Pro.local>
2015-05-16 18:29:54 -0400

    back button functioning

--- src/app/cars/main.controller.js
+++ src/app/cars/main.controller.js
@@ -1,10 +1,8 @@
 'use strict';
 
 angular.module('idlecars')
-.controller('main.controller', function ($scope, mapService, $location) {
+.controller('main.controller', function ($scope) {
   $scope.showBack = true;
   $scope.done = true;
-
-  console.log($location.path())
 
 })
--- src/components/navbar/navbar.controller.js
+++ src/components/navbar/navbar.controller.js
@@ -1,8 +1,23 @@
 'use strict';
 
 angular.module('idlecars')
-  .controller('navbar.controller', function ($scope, $location) {
+  .controller('navbar.controller', function ($scope, $location, $stateParams) {
     $scope.goBack = function() {
-      window.history.back();
+
+      var currentPath = $location.path();
+      var last = currentPath[currentPath.length - 1];
+      var destination;
+
+      if (last == 'g') {
+        destination = 'http://localhost:3000/#/cars/' + $stateParams.carId;
+      }
+      else if (!isNaN(last)) {
+        destination = 'http://localhost:3000';
+      }
+      else {
+        destination = 'http://localhost:8000';
+      };
+
+      window.location.replace(destination);
     }
   });
