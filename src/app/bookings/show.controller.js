cf09f1259239e99b3f235cf7268c596d6fe18996
jefk <speedracer111@gmail.com>
2015-05-29 20:43:48 -0400

    janky file upload

--- src/app/bookings/show.controller.js
+++ src/app/bookings/show.controller.js
@@ -2,6 +2,25 @@
 
 angular.module('idlecars')
 .controller('bookings.showCtrl', function ($scope, Upload) {
+  Parse.initialize("ltjv77iZ50v0g4Czpd6nbcxGgNb0McnPdJ5v5kcj", "zncD5UiutU0xRXbG9DCTYSHP6XmBGykSRYzxiq8K");
+
+  var UserUploads = Parse.Object.extend('UserUploads');
+  var query = new Parse.Query(UserUploads);
+  var userUploads;
+
+  $scope.uploadUrl = null;
+
+  query.get("Fmcb73mqWY").done(function (object) {
+    userUploads = object;
+    var imageUrl = userUploads.get('driver_license_image').url();
+    console.log(userUploads.get('driver_license_image'));
+    if (imageUrl) {
+      console.log(imageUrl);
+      $scope.uploadUrl = imageUrl;
+      $scope.$apply();
+    }
+  });
+
   $scope.upload = function(files) {
     var file = files[0];
     if (!file) {
@@ -9,9 +28,22 @@ angular.module('idlecars')
       return;
     }
 
-    Upload.upload({
-      url: '/foo',
-      file: file,
-    });
+    var file = new Parse.File(file.name, file, file.type);
+
+    file.save().then(
+      function() {
+        userUploads.set("driver_license_image", file);
+        userUploads.save().then(function(uploads) {
+          $scope.uploadUrl = uploads.get('driver_license_image').url()
+          console.log($scope.uploadUrl);
+          $scope.$apply();
+        });
+      },
+      handleError
+    );
   };
+
+  var handleError = function(error) {
+    console.log(error);
+  }
 });
