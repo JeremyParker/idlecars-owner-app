ba2db0a46a5c9b09c0fba90bf43f530270ec70fe
jefk <speedracer111@gmail.com>
2015-06-02 11:44:58 -0400

    move upload to a service

--- src/app/bookings/show.controller.js
+++ src/app/bookings/show.controller.js
@@ -1,29 +1,28 @@
 'use strict';
 
 angular.module('idlecars')
-.controller('bookings.showCtrl', function ($scope, Upload, UserUploadService) {
+.controller('bookings.showCtrl', function ($scope, $timeout, Upload, UserUploadService) {
+  var user = {parse_id: 'Fmcb73mqWY'};
 
-  $scope.uploadUrl = null;
+  var _setImageUrl = function(usersUploads) {
+    // NOTE: $timeout makes sure that this "runs on the next digest cycle"
+    // I don't really know what that means, but this works
+    $timeout(function() {
+      $scope.uploadUrl = usersUploads.get('driver_license_image').url();
+    });
+  };
 
-  UserUploadService.fileUrl({
-    user: {parse_id: 'Fmcb73mqWY'},
-    fileCategory: 'driver_license_image',
-  }).then(function(url) {
-    $scope.uploadUrl = url;
-  });
+  UserUploadService.get({
+    user: user,
+  }).then(_setImageUrl);
 
   $scope.upload = function(files) {
     var file = files[0];
-    if (!file) {
-      console.log('no file');
-      return;
-    }
+    if (!file) { return; }
 
     UserUploadService.upload({
       user: user,
       file: file,
-    }).then(function() {
-      console.log('done');
-    });
+    }).then(_setImageUrl);
   };
 });
--- src/components/user_uploads/service.js
+++ src/components/user_uploads/service.js
@@ -5,50 +5,29 @@ angular.module('idlecars')
   Parse.initialize("ltjv77iZ50v0g4Czpd6nbcxGgNb0McnPdJ5v5kcj", "zncD5UiutU0xRXbG9DCTYSHP6XmBGykSRYzxiq8K");
   var UserUploads = Parse.Object.extend('UserUploads');
 
-  var userUploads;
-
   var upload = function(options) {
-    promise = $q(function(resolve, reject) {
-      resolve();
-    });
-    return promise;
-  }
+    var file = options.file;
+    var parseFile = new Parse.File(file.name, file, file.type);
 
-  var fileUrl = function(options) {
-    promise = $q(function(resolve, reject) {
-      var query = new Parse.Query(UserUploads);
-      query.get(options.user.parse_id).done(function (object) {
-        userUploads = object;
-        var imageUrl = userUploads.get('driver_license_image').url();
-        if (imageUrl) {
-          resolve(imageUrl);
-        }
+    // NOTE: gross
+    return $q(function(resolve, reject) {
+      parseFile.save().then(function() {
+        getUsersUploads(options).then(function(usersUploads) {
+          usersUploads.set("driver_license_image", parseFile);
+          resolve(usersUploads.save());
+        });
       });
     });
-    return promise;
+  };
+
+  var getUsersUploads = function(options) {
+    // TODO: cache this value, but how when it's async?
+    var query = new Parse.Query(UserUploads);
+    return query.get(options.user.parse_id);
   };
 
   return {
     upload: upload,
-    fileUrl: fileUrl
+    get: getUsersUploads,
   };
-
-    // var file = new Parse.File(file.name, file, file.type);
-
-    // file.save().then(
-    //   function() {
-    //     userUploads.set("driver_license_image", file);
-    //     userUploads.save().then(function(uploads) {
-    //       $scope.uploadUrl = uploads.get('driver_license_image').url()
-    //       console.log($scope.uploadUrl);
-    //       $scope.$apply();
-    //     });
-    //   },
-    //   handleError
-    // );
-
-  var handleError = function(error) {
-    console.log(error);
-  }
-
 });
