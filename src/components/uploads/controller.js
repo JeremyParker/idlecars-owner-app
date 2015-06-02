486c92af274704515ea8dcabbc59548dd4a6ab78
jefk <speedracer111@gmail.com>
2015-06-02 16:45:08 -0400

    loading indicator

--- src/components/uploads/controller.js
+++ src/components/uploads/controller.js
@@ -4,6 +4,7 @@ angular.module('idlecars')
 .controller('upload.controller', function($scope, $timeout, Upload, UserUploadService) {
   var user = {parse_id: 'Fmcb73mqWY'};
   $scope.fileUrl = false;
+  $scope.isBusy = false;
 
   var _setImageUrl = function(usersUploads) {
     // NOTE: $timeout makes sure that this "runs on the next digest cycle"
@@ -11,6 +12,7 @@ angular.module('idlecars')
     $timeout(function() {
       var file = usersUploads.get($scope.fileKey);
       if (file) { $scope.fileUrl = file.url(); }
+      $scope.isBusy = false;
     });
   };
 
@@ -21,6 +23,8 @@ angular.module('idlecars')
   $scope.upload = function(files) {
     var file = files[0];
     if (!file) { return; }
+
+    $scope.isBusy = true;
 
     UserUploadService.upload({
       user: user,
