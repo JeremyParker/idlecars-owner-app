170d3505c68e9f468e0dd96d4e734838a21dac97
jefk <speedracer111@gmail.com>
2015-06-04 13:40:20 -0400

    add test for booking

--- e2e/cars/booking.po.js
+++ e2e/cars/booking.po.js
@@ -2,6 +2,12 @@
 
 var CarsBooking = function() {
   this.header = element(by.css('h2'));
+  this.nextButton = element(by.cssContainingText('button', 'next>'));
+
+  this.emailInput = element(by.css('input[name=email]'));
+  this.firstNameInput = element(by.css('input[name=first_name]'));
+  this.lastNameInput = element(by.css('input[name=last_name]'));
+  this.phoneInput = element(by.css('input[name=phone_number]'));
 };
 
 module.exports = new CarsBooking();
---
+++ e2e/cars/booking_show.po.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// TODO: move to a bookings folder
+var BookingsDetail = function() {
+  this.header = element(by.css('h1'));
+};
+
+module.exports = new BookingsDetail();
