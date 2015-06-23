c4e6af51d4603b1408d586f5d1b9842009bdbf31
jefk <speedracer111@gmail.com>
2015-06-23 12:46:14 -0400

    notify about wrong password

--- src/components/app_notifications/interceptor.js
+++ src/components/app_notifications/interceptor.js
@@ -5,12 +5,18 @@ angular.module('idlecars')
   var interceptor = {};
 
   interceptor.responseError = function(rejection) {
-    var notifications = rejection.data._app_notifications || [];
+    var notifications = _notifications(rejection);
     for (var i = 0; i < notifications.length; i++) {
       AppNotificationService.push(notifications[i]);
     }
 
     return $q.reject(rejection);
+  }
+
+  var _notifications = function(rejection) {
+    if (!rejection.data) { return []; }
+
+    return rejection.data._app_notifications || [];
   }
 
   return interceptor;
