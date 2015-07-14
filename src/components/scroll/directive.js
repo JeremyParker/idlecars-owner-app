ba9cb6be94cfad28e8b193b549e48a507c63f0af
craigstar <craig@idlecars.com>
2015-07-14 09:33:28 -0400

    hacky way. use $invterval to scroll

--- src/components/scroll/directive.js
+++ src/components/scroll/directive.js
@@ -1,17 +1,20 @@
 'use strict';
 
 angular.module('idlecars')
-.directive('scroll', function ($timeout, $state, ScrollService) {
+.directive('scroll', function ($interval, $state, ScrollService) {
   return {
     restrict: 'A',
     link: function (scope, element, attrs) {
-      var position = ScrollService.scrollPosition[$state.current.name];
+      var position = ScrollService.scrollPosition[$state.current.name] || 0;
 
-      scope.$on('$viewContentLoaded', function(){
-        $timeout(function () {
-          element[0].scrollTop = position;
-        }, 500)
-      });
+      var scrollToPosition = $interval(function () {
+        element[0].scrollTop = position;
+        console.log(element)
+
+        if (element[0].scrollTop >= position) {
+          $interval.cancel(scrollToPosition);
+        };
+      })
 
       element[0].onscroll = function () {
         ScrollService.scrollPosition[$state.current.name] = element[0].scrollTop;
