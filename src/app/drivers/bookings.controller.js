792265d7a2b1c445d7c2303ef328a9b5c81315e0
craigstar <craig@idlecars.com>
2015-07-15 16:16:24 -0400

    get user name

--- src/app/drivers/bookings.controller.js
+++ src/app/drivers/bookings.controller.js
@@ -1,17 +1,19 @@
 'use strict';
 
 angular.module('idlecars')
-.controller('bookings.controller', function ($scope, Restangular) {
+.controller('bookings.controller', function ($scope, Restangular, MyDriverService) {
+
+  var initScope = function (me) {
+    var name = me.first_name + me.last_name;
+    $scope.username = name || me.phone_number;
+  }
+
+  MyDriverService.get().then(initScope);
 
   Restangular.all('bookings').getList().then(function (bookings) {
     var booking = bookings[0];
 
-    $scope.username = 'Brian Claypool';
-
     $scope.car = booking.car;
     $scope.state = booking.state;
   })
-
-
-
 })
