5744991d720f3866f14611ec3ac7e58f1b32f6ab
jefk <speedracer111@gmail.com>
2015-07-20 16:09:50 -0700

    toggle filter

--- src/components/filters/range.filter.js
+++ src/components/filters/range.filter.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('icFilters')
+
 .filter('attrInRange', function() {
   return function(items, attrKey, min, max) {
     if (!items) { return []; }
@@ -9,4 +10,31 @@ angular.module('icFilters')
       return item[attrKey] >= min && item[attrKey] < max;
     });
   }
-});
+})
+
+.filter('attrFilter', function() {
+  var _anyFiltersOn = function(attrs) {
+    for (var key in attrs) {
+      if (attrs[key]) { return true; }
+    }
+  }
+
+  return function(items, attrs) {
+    console.log(attrs)
+    if (!items) { return items; }
+    if (!attrs) { return items; }
+    if (!_anyFiltersOn(attrs)) { return items; }
+
+    var matches = [];
+    for (var i in items) {
+      for (var key in items[i]) {
+        if (items[i][key] && attrs[key]) {
+          matches.push(items[i])
+        }
+      }
+    }
+
+    return matches;
+  }
+})
+;
