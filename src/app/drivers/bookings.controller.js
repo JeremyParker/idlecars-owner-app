d6113fcbe7a7b61fa703717e380eb7562a253e2d
craigstar <craig@idlecars.com>
2015-07-21 15:00:03 -0400

    cancel button back

--- src/app/drivers/bookings.controller.js
+++ src/app/drivers/bookings.controller.js
@@ -3,6 +3,8 @@
 angular.module('idlecars')
 .controller('bookings.controller', function ($scope, Restangular, MyDriverService) {
 
+  var bookingId;
+
   var initScope = function (me) {
     $scope.username = me.client_display;
   }
@@ -11,8 +13,13 @@ angular.module('idlecars')
 
   Restangular.all('bookings').getList().then(function (bookings) {
     var booking = bookings[0];
+    bookingId = booking.id;
 
     $scope.car = booking.car;
     $scope.state = booking.state_details;
   })
+
+  $scope.cancelBooking = function () {
+
+  }
 })
