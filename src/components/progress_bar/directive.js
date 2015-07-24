90df1ea06b389808615ef52b86d8d3de48a8a79b
craigstar <craig@idlecars.com>
2015-07-23 23:00:03 -0400

    add a controller to deal with two conditions

--- src/components/progress_bar/directive.js
+++ src/components/progress_bar/directive.js
@@ -4,6 +4,10 @@ angular.module('idlecars')
 .directive('progressBar', function () {
   return {
     templateUrl: 'components/progress_bar/template.html',
+    controller: function ($scope) {
+      if($scope.steps){ $scope.completion = $scope.step/$scope.steps*100 + '%'};
+      if($scope.progress){ $scope.completion = $scope.progress + '%' };
+    },
     scope: {
       progress: '@',
       steps: '@',
