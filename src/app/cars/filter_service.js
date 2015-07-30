83a2a14ca00f6db0ec8d45a46eb0160c12e3d9f5
jefk <speedracer111@gmail.com>
2015-07-29 18:24:02 -0700

    generify the filtering

--- src/app/cars/filter_service.js
+++ src/app/cars/filter_service.js
@@ -4,11 +4,13 @@ angular.module('idlecars')
 .factory('CarFilterService', function() {
   var service = {
     allCars: [],
-    costFilter: {},
+    filters: {},
   };
 
-  service.filterCost = function(setting) {
-    service.costFilter[setting] = !service.costFilter[setting];
+  service.filter = function(feature, setting) {
+    if (!service.filters[feature]) { service.filters[feature] = {}; }
+
+    service.filters[feature][setting] = !service.filters[feature][setting];
     return service.filterCars();
   }
 
@@ -16,13 +18,19 @@ angular.module('idlecars')
     if (!_anyFiltersOn()) { return service.allCars; }
 
     return service.allCars.filter(function(car) {
-      return service.costFilter[car.cost_bucket];
+      for (var feature in car.searchable) {
+        var feature_filter = service.filters[feature];
+        if (feature_filter && feature_filter[car.searchable[feature]]) { return true; }
+      }
+      return false;
     });
   }
 
   var _anyFiltersOn = function() {
-    for (var key in service.costFilter) {
-      if (service.costFilter[key]) { return true; }
+    for (var feature in service.filters) {
+      for (var setting in service.filters[feature]) {
+        if (service.filters[feature][setting]) { return true; }
+      }
     }
   }
 
