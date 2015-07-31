8567c19bbff48015193a1f71eeaf10a4e1ff984a
craigstar <craig@idlecars.com>
2015-07-31 14:18:58 -0400

    button works

--- src/app/auth/new_user/change_password.controller.js
+++ src/app/auth/new_user/change_password.controller.js
@@ -1,7 +1,23 @@
 'use strict';
 
 angular.module('idlecars')
-.controller('auth.changePassword.controller', function ($scope) {
+.controller('auth.changePassword.controller', function ($scope, $state, Restangular, MyDriverService, AppNotificationService) {
   $scope.label = 'change my password';
   $scope.button = 'Send email';
+
+  $scope.buttonClick = function () {
+    MyDriverService.get().then(function (me) {
+      var postParams = { phone_number: me.phone_number };
+      var passwordReset = Restangular.all('password').all('reset_setups');
+      passwordReset.post(postParams)
+      .then(function() {
+        // TODO(JP) - hook up SMS service and $state.go to the SMS screen that @craigstar made.
+        return $state.go('driverAccount');
+      })
+      .then(function() {
+        AppNotificationService.push('An email has been sent to your address. Please check your email.');
+      });
+    }
+  )
+  }
 })
