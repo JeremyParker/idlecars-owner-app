2015a7bc06f1850cae46501a2b1c9381c4a98fdf
Jeremy Parker <github@jeremyparker.org>
2015-08-01 17:45:54 -0400

    make sure users dont get stuck entering phone numbers

--- src/components/format_tel/directive.js
+++ src/components/format_tel/directive.js
@@ -10,6 +10,16 @@ angular.module('idlecars')
       element[0].oninput = function (event) {
         if (!needToFormat) { return; };
 
+        // special-cases for users who add the right formatting themselves
+        if (5 == element[0].value.length && ')' == element[0].value.slice(-1))
+          return
+
+        if (6 == element[0].value.length && ' ' == element[0].value.slice(-1))
+          return
+
+        if (10 == element[0].value.length && '-' == element[0].value.slice(-1))
+          return
+
         var value = element[0].value.replace(/[^0-9]/g, '')
 
         switch (value.length) {
@@ -23,7 +33,7 @@ angular.module('idlecars')
           case 4:
           case 5:
           case 6:
-            value = '(' + value.substr(0, 3) + ') ' + value.substr(3, 3);
+            value = '(' + value.substr(0, 3) + ') ' + value.substr(3);
             break;
           default:
             value = '(' + value.substr(0, 3) + ') ' + value.substr(3, 3) + '-' + value.substr(6);
