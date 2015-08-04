cb7397d1f521e6778d8510f482d314d37eeee8e8
jefk <speedracer111@gmail.com>
2015-08-04 09:53:37 -0700

    toggle body type

--- src/app/cars/filter_service.js
+++ src/app/cars/filter_service.js
@@ -11,6 +11,7 @@ angular.module('idlecars')
     if (!service.filters[feature]) { service.filters[feature] = {}; }
 
     service.filters[feature][setting] = !service.filters[feature][setting];
+    _toggleBodyType(feature, setting);
     return service.filterCars();
   }
 
@@ -32,6 +33,16 @@ angular.module('idlecars')
       for (var setting in service.filters[feature]) {
         if (service.filters[feature][setting]) { return true; }
       }
+    }
+  }
+
+  var _toggleBodyType = function(feature, bodyType) {
+    if (feature !== 'body_type') { return }
+    if (bodyType === 'Sedan') {
+      service.filters.body_type.SUV = false
+    }
+    if (bodyType === 'SUV') {
+      service.filters.body_type.Sedan = false
     }
   }
 
