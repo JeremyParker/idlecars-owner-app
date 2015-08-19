59e4a6d08fd8f9611ac34a62be50857f13ee91a8
craigstar <craig@idlecars.com>
2015-08-19 15:34:35 -0400

    basic date picker

--- src/components/rental_date/diretive.js
+++ src/components/rental_date/diretive.js
@@ -4,5 +4,16 @@ angular.module('idlecars')
 .directive('rentalDate', function () {
   return {
     templateUrl: 'components/rental_date/template.html',
+    controller: function ($scope) {
+      $scope.options = {
+        clear: '',
+        today: '',
+        min: 7,
+        weekdaysShort: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+        onClose: function () {
+          console.log($scope.date.getMonth())
+        }
+      }
+    }
   }
 })
