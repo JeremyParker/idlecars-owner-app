33a01f1248c94c69afae61e85ba1b65bd571d642
jefk <speedracer111@gmail.com>
2015-08-20 10:47:44 -0700

    start the form

---
+++ src/app/owners/routes.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('idlecars')
+.config(function ($stateProvider) {
+
+  $stateProvider
+
+    .state('bankLink', {
+      url: '/bank-link',
+      data: {navbarInfo: {title: 'Link Bank Account', enableBack: true}},
+      views: {
+        'navbar@': {
+          templateUrl: 'components/navbar/navbar_main.html',
+          controller: 'navbarMain.controller',
+        },
+        'content@': {
+          templateUrl: 'app/owners/bank_link.html',
+        }
+      }
+    })
+
+})
