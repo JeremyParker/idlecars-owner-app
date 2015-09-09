6b160983e955bd19aeefe2f3d221f3aeffa1c609
craigstar <craig@idlecars.com>
2015-09-09 11:09:42 -0400

    stored in local storage

--- src/components/tutorial/directive.js
+++ src/components/tutorial/directive.js
@@ -3,9 +3,19 @@
 angular.module('idlecars')
 .directive('tutorial', function () {
   return {
+    scope: {},
     templateUrl: 'components/tutorial/template.html',
-    controller: function ($scope, $localStorage) {
-      console.log($localStorage)
+    controller: function ($scope, $localStorage, $sessionStorage) {
+      $scope.tutorialOn = !$sessionStorage.haveSeenTutorial && !$localStorage.tutorialsetting;
+
+      $scope.closeTutorial = function () {
+        $scope.tutorialOn = false;
+        $sessionStorage.haveSeenTutorial = true;
+      }
+
+      $scope.setLocalStorage = function (setting) {
+        $localStorage.tutorialsetting = setting;
+      }
     }
   }
 })
