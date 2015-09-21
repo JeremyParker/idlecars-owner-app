5cde43fbf59c7ec900c71e24bd7d2dddc8fe0c3c
craigstar <craig@idlecars.com>
2015-09-21 15:20:08 -0400

    separate or/and filters

--- src/app/cars/list.controller.js
+++ src/app/cars/list.controller.js
@@ -3,7 +3,8 @@
 angular.module('idlecars')
 .controller('cars.list.controller', function ($scope, $timeout, CarService, CarFilterService) {
   $timeout(function() {
-    $scope.filters = CarFilterService.filters || {};
+    $scope.orFilters = CarFilterService.orFilters || {};
+    $scope.andFilters = CarFilterService.andFilters || {};
   });
 
   CarService.query().$promise.then(function(cars) {
@@ -11,8 +12,17 @@ angular.module('idlecars')
     $scope.cars = CarFilterService.filterCars();
   });
 
-  $scope.didFilter = function(feature, setting, $event) {
+  $scope.addOrFilter = function(feature, setting, $event) {
     if ($event) { $event.stopPropagation(); }
-    $scope.cars = CarFilterService.filter(feature, setting);
+    CarFilterService.orFilter(feature, setting);
+  }
+
+  $scope.addAndFilter = function(feature, setting, $event) {
+    if ($event) { $event.stopPropagation(); }
+    CarFilterService.andFilter(feature, setting);
+  }
+
+  $scope.filter = function () {
+    $scope.cars = CarFilterService.filterCars();
   }
 })
