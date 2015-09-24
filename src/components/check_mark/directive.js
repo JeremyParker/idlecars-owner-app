9ed2a682570ca8c325353bb1b5dd4ab1fa365aed
craigstar <craig@idlecars.com>
2015-09-24 18:02:43 -0400

    beautiful checkbox with TOS

--- src/components/check_mark/directive.js
+++ src/components/check_mark/directive.js
@@ -4,7 +4,8 @@ angular.module('idlecars')
 .directive('checkMark', function () {
   return {
     scope: {
-      checked: '=',
+      status: '=',
+      checked: '@',
     },
     transclude: true,
     templateUrl: 'components/check_mark/check_mark.html',
