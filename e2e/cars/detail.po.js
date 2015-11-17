8c278e89ce294fb70558561c609448c223aebd8b
craigstar <craig@idlecars.com>
2015-11-17 18:19:49 -0500

    deleted driver e2e

--- e2e/cars/detail.po.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var CarsDetail = function() {
-  this.header = element(by.css('.headline-features h2'));
-  this.bookingLink = element(by.css('.floater .request'));
-  this.helpButton = element(by.css('.floater .help'));
-};
-
-module.exports = new CarsDetail();
--- e2e/cars/renewal.po.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var RenewalsShow = function() {
-  this.message = element(by.css('.modal-content p'));
-};
-
-module.exports = new RenewalsShow();
