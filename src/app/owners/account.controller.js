fbc077d655461f94216a04614c8ebdfda875dbf1
craigstar <craig@idlecars.com>
2015-11-18 16:43:24 -0500

    account page user owner hookup with server

--- src/app/owners/account.controller.js
+++ src/app/owners/account.controller.js
@@ -1,15 +1,23 @@
 'use strict';
 
 angular.module('idlecars')
-.controller('account.controller', function ($scope) {
+.controller('account.controller', function ($scope, UserService, MyOwnerService) {
 
   // TODO: we should move it to a presenter.
-  $scope.accountInfo = [
-    {title: 'First Name', link: '.update.firstname', content: 'first_name'},
-    {title: 'Last Name', link: '.update.lastname', content: 'last_name'},
-    {title: 'Email', link: '.update.email', content: 'email'},
-    {title: 'Phone number', link: '.update.phonenumber', content: 'phone_number'},
+  $scope.userInfo = [
+    {title: 'First Name', link: 'user.firstname', content: 'first_name'},
+    {title: 'Last Name', link: 'user.lastname', content: 'last_name'},
+    {title: 'Email', link: 'user.email', content: 'email'},
+    {title: 'Phone number', link: 'user.phonenumber', content: 'phone_number'},
     {title: 'Password', link: 'password.change', content: ''},
+  ];
+
+  $scope.ownerInfo = [
+    {title: 'Company Name', link: 'user.firstname', content: 'company_name'},
+    {title: 'Address', link: 'user.lastname', content: 'address1'},
+    {title: 'Apartment', link: 'user.email', content: 'address2'},
+    {title: 'City', link: 'user.phonenumber', content: 'city'},
+    {title: 'Zip Code', link: 'password.change', content: 'zipcode'},
   ];
 
   $scope.driverDocuments = [
@@ -18,4 +26,12 @@ angular.module('idlecars')
     {title: 'Defensive Driving', link: '.update.uploadDefensiveCert', image: 'defensive_cert_image'},
     {title: 'Proof of Address', link: '.update.uploadAddressProof', image: 'address_proof_image'}
   ];
+
+  UserService.get().then(function (user) {
+    $scope.user = user;
+  })
+
+  MyOwnerService.get().then(function (owner) {
+    $scope.owner = owner;
+  })
 })
