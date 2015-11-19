c47c97b901852a90ed60f7b8e9daa1934af2147b
craigstar <craig@idlecars.com>
2015-11-19 12:32:09 -0500

    bank link setup

--- src/app/bank/routes.js
+++ src/app/bank/routes.js
@@ -5,34 +5,15 @@ angular.module('idlecars')
 
   $stateProvider
 
-    .state('bankLink', {
+    .state('ownerAccount.bankLink', {
       url: '/bank-link',
-      data: {navbarInfo: {title: 'Link Bank Account'}},
+      data: {navbarInfo: {title: 'Link Bank Account', enableBack: true}},
       views: {
-        // TODO extend some base object, or have an `app` base state that holds the navbar
-        'navbar@': {
-          templateUrl: 'components/navbar/navbar_main.html',
-          controller: 'navbarMain.controller',
-        },
         'content@': {
           templateUrl: 'app/owners/bank_link.html',
           controller: 'owners.bankLink.controller'
         }
       }
-    })
-
-    .state('bankSuccess', {
-      url: '/bank_success',
-      data: {navbarInfo: {title: ''}},
-      views: {
-        'navbar@': {
-          templateUrl: 'components/navbar/navbar_main.html',
-          controller: 'navbarMain.controller',
-        },
-        'content@': {
-          templateUrl: 'app/owners/success.html',
-        },
-      },
     })
 
 })
