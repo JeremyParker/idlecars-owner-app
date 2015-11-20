475567ae64d286d69d9c555e9a1b41657c16a203
craigstar <craig@idlecars.com>
2015-11-20 14:33:38 -0500

    get rid of mock up

--- src/app/cars/choice.controller.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('idlecars')
-.controller('choice.controller', function ($scope) {
-  $scope.formTitle = 'something here'
-  $scope.choices = ['black', 'red', 'white']
-})
