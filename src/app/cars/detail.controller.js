4c80607432404ea71491ae1130968913ebc6728b
craigstar <craig@idlecars.com>
2015-11-23 14:59:45 -0500

    more fields

--- src/app/cars/detail.controller.js
+++ src/app/cars/detail.controller.js
@@ -20,9 +20,13 @@ angular.module('idlecars')
     {title: 'Weekly rent', link: '.update.rent', content: 'solo_cost'},
     {title: 'Available', link: '.update.available', content: 'next_available_date'},
     {title: 'Deposit', link: '.update.deposit', content: 'solo_deposit'},
-    {title: 'Deposit', link: '.update.deposit', content: 'solo_deposit'},
-    {title: 'Deposit', link: '.update.deposit', content: 'solo_deposit'},
-    {title: 'Deposit', link: '.update.deposit', content: 'solo_deposit'},
+    {title: 'Min rental', link: '.update.minimum', content: 'min_lease'},
+    {title: 'Mileage', link: '.update.mileage', content: 'last_known_mileage'},
+    {title: 'Exterior color', link: '.update.exterior', content: 'exterior_color'},
+    {title: 'Interior color', link: '.update.interior', content: 'interior_color'},
   ];
 
+  $scope.delist = function () {
+    // TODO: an endpoint to delist car
+  }
 })
