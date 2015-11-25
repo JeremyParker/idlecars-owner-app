a356312f6e92b0c256707f084d4fe10d08ec74de
craigstar <craig@idlecars.com>
2015-11-25 13:52:02 -0500

    display weeks and months

--- src/app/cars/service.js
+++ src/app/cars/service.js
@@ -15,18 +15,17 @@ angular.module('idlecars')
   ];
 
   service.minimum_lease = [
-    {key: '_00_unknown', value: 'Unknown'},
-    {key: '_01_no_min', value: 'No'},
-    {key: '_02_one_week', value: '7 days'},
-    {key: '_03_two_weeks', value: '14 days'},
-    {key: '_04_three_weeks', value: '21 days'},
-    {key: '_05_one_month', value: '30 days'},
-    {key: '_06_six_weeks', value: '45 days'},
-    {key: '_07_two_months', value: '60 days'},
-    {key: '_08_three_months', value: '90 days'},
-    {key: '_09_four_months', value: '120 days'},
-    {key: '_10_five_months', value: '150 days'},
-    {key: '_11_six_months', value: '180 days'},
+    {key: '_01_no_min', value: 'No minimum'},
+    {key: '_02_one_week', value: 'One week'},
+    {key: '_03_two_weeks', value: 'Two weeks'},
+    {key: '_04_three_weeks', value: 'Three weeks'},
+    {key: '_05_one_month', value: 'One month'},
+    {key: '_06_six_weeks', value: 'Six weeks'},
+    {key: '_07_two_months', value: 'Two months'},
+    {key: '_08_three_months', value: 'Three months'},
+    {key: '_09_four_months', value: 'Four months'},
+    {key: '_10_five_months', value: 'Five months'},
+    {key: '_11_six_months', value: 'Six months'},
   ];
 
   service.get = function (carId) {
