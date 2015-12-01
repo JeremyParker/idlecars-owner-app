bd75b73a83f410e58027cf6cab297908368e1ffd
Jeremy Parker <github@jeremyparker.org>
2015-11-30 20:17:32 -0500

    reorder some stuff in car detail screen

--- src/app/cars/detail.controller.js
+++ src/app/cars/detail.controller.js
@@ -24,9 +24,9 @@ angular.module('idlecars')
 
   $scope.carDetail = [
     {title: 'Weekly rent', link: getLink('rent'), content: 'solo_cost'},
-    {title: 'Available', link: getLink('available'), content: 'available_date_display'},
     {title: 'Deposit', link: getLink('deposit'), content: 'solo_deposit'},
     {title: 'Min rental', link: getLink('minimum'), content: 'min_lease_display'},
+    {title: 'Available', link: getLink('available'), content: 'available_date_display'},
     {title: 'Mileage', link: getLink('mileage'), content: 'last_known_mileage'},
     {title: 'Exterior color', link: getLink('exterior'), content: 'exterior_color'},
     {title: 'Interior color', link: getLink('interior'), content: 'interior_color'},
@@ -40,7 +40,7 @@ angular.module('idlecars')
 
   $scope.relistCar = function () {
     $scope.isBusy = true;
-    CarService.patch($scope.car.id, {status: 'available'})
+    CarService.patch($scope.car.id, {next_available_date: [2015, 0, 1]})
     .then(function (car) {
       $scope.car = car;
       var message = "You've successfully relisted your " + car.name + " " + car.plate;
@@ -52,7 +52,7 @@ angular.module('idlecars')
 
   $scope.delistCar = function () {
     $scope.isBusy = true;
-    CarService.patch($scope.car.id, {status: 'busy'})
+    CarService.patch($scope.car.id, {next_available_date: []})
     .then(function (car) {
       $scope.car = car;
       var message = "You've delisted your " + car.name + " " + car.plate;
