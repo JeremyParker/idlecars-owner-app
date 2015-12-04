3c5526c2edf35acd841036e4f63264d1dc8a071f
craigstar <craig@idlecars.com>
2015-12-04 13:36:38 -0500

    cars page requires completion

--- src/app/cars/cars.controller.js
+++ src/app/cars/cars.controller.js
@@ -1,8 +1,21 @@
 'use strict';
 
 angular.module('idlecars')
-.controller('cars.controller', function ($scope, CarService) {
+.controller('cars.controller', function ($scope, $state, CarService, OwnerRequiredService, UserRequiredService, AppNotificationService) {
   CarService.get().then(function (cars) {
     $scope.cars = cars
   })
+
+  var checkStates = function (state) {
+    $scope.isBusy = false;
+
+    if (state) {
+      $state.go('ownerAccount');
+      AppNotificationService.push({warning: 'Remember to upload your missing documents. Go to "My Account" from the menu.'})
+    };
+  }
+
+  $scope.isBusy = true;
+  UserRequiredService.requiredDocState().then(checkStates);
+  OwnerRequiredService.requiredDocState().then(checkStates);
 })
