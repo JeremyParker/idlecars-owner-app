201dccd1ae09688e03372c613d9fbfbe1477bf81
craigstar <craig@idlecars.com>
2015-12-07 17:52:37 -0500

    account page

---
+++ e2e/owner/account.po.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var Account = function() {
+  var self = this;
+
+  self.userInfo = element.all(by.repeater('item in userInfo'));
+  self.firstname = self.userInfo.get(0).element(by.css('.content-main'));
+
+  self.ownerInfo = element.all(by.repeater('item in ownerInfo'));
+  self.company = self.ownerInfo.get(0).element(by.css('.content-main'));
+
+  self.smsButton = element.all(by.css('on-off-button'));
+
+  self.logoutButton = element(by.css('.alert-button'));
+};
+
+module.exports = new Account();
