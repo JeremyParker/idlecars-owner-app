920d72966227f70376b2029d145d195ddfd5d4b2
craigstar <craig@idlecars.com>
2015-12-08 11:10:08 -0500

    cta buttons

---
+++ e2e/spec/CTA_buttons.spec.js
@@ -0,0 +1,29 @@
+'use strict';
+
+describe('CTA buttons should work:', function () {
+  var cars = require('../cars/cars.po');
+  var detail = require('../cars/detail.po');
+  var navbar = require('../components/navbar.po');
+  var fields = require('../templates/fields.po');
+  var login = require('../auth/login.po');
+  var helpers = require('../spec_helper');
+
+  beforeEach(function () {
+    helpers.startTest();
+    login.setToken('owner_has_bank_link');
+    browser.get('http://localhost:3001/#/cars/1');
+  });
+
+  it('remove list and relist ', function () {
+    expect(detail.status.getText()).toContain('Listed');
+    expect(detail.ctaButtons.count()).toBe(1);
+    detail.firstCtaButton.click();
+
+    navbar.saveButton.click();
+    expect(detail.status.getText()).toContain('Not listed');
+    detail.firstCtaButton.click();
+
+    navbar.saveButton.click();
+    expect(detail.status.getText()).toContain('Listed');
+  });
+})
