997db1a53b32b17b13d8d16d2bb0cde825febd1a
craigstar <craig@idlecars.com>
2016-01-28 11:51:35 -0500

    loading screen

--- src/components/car_detail/directive.js
+++ src/components/car_detail/directive.js
@@ -10,12 +10,15 @@ angular.module('idlecars')
     templateUrl: 'components/car_detail/template.html',
     controller: function ($scope, $state, CarService) {
       $scope.go = function (key) {
+        $scope.isBusy = true;
+
         if (key == 'AddBankLink') {
           $state.go('ownerAccount.bankLink');
         }
         else if (key == 'RemoveListing') {
           CarService.patch($scope.car.id, {next_available_date: null})
           .then(function (car) { $state.go('cars.update.available', {carId: $scope.car.id}) })
+          .finally(function () { $scope.isBusy = false })
         }
         else if (key == 'Relist') {
           var date = new Date();
@@ -23,24 +26,30 @@ angular.module('idlecars')
 
           CarService.patch($scope.car.id, {next_available_date: today})
           .then(function (car) { $state.go('cars.update.available', {carId: $scope.car.id}) } )
+          .finally(function () { $scope.isBusy = false })
         }
         else if (key == 'GoNextAvailableDate') {
           $state.go('cars.update.available', {carId: $scope.car.id})
         }
         else if (key == 'RenewListing') {
-          CarService.renew($scope.car.id).then(function (car) { $scope.car = car })
+          CarService.renew($scope.car.id)
+          .then(function (car) { $scope.car = car })
+          .finally(function () { $scope.isBusy = false })
         }
         else if (key == 'ApproveInsurance') {
           CarService.patch($scope.car.id, {insurance: 'approved'})
           .then(function (car) { $scope.car = car } )
+          .finally(function () { $scope.isBusy = false })
         }
         else if (key == 'RejectInsurance') {
           CarService.patch($scope.car.id, {insurance: 'rejected'})
           .then(function (car) { $scope.car = car } )
+          .finally(function () { $scope.isBusy = false })
         }
         else if (key == 'ReturnConfirm') {
           CarService.patch($scope.car.id, {return_confirmation: ''})
           .then(function (car) { $scope.car = car } )
+          .finally(function () { $scope.isBusy = false })
         }
       }
     }
