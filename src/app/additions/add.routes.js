d5af031e53fe211c6d0bbeea95929fa4080af0c1
Jeremy Parker <github@jeremyparker.org>
2016-04-18 22:47:30 -0400

    stuff to add driver

---
+++ src/app/additions/add.routes.js
@@ -0,0 +1,53 @@
+'use strict';
+
+angular.module('idlecars')
+.config(function ($stateProvider) {
+
+  $stateProvider
+
+    .state('additions.add', {
+      abstract: true,
+      url: 'additions/add/:additionId',
+      views: {
+        'content@': {
+          template: '<ui-view class="flex"/>',
+          controller: 'additions.add.controller',
+        },
+      },
+    })
+
+    .state('additions.add.phone_number', {
+      url: '/phone_number',
+      data: {navbarInfo: {title: 'Phone Number', enableBack: true, enableNext: true}},
+      templateUrl: 'shared/users/form.html',
+      controller: 'additions.add.phone_number.controller',
+    })
+
+    .state('additions.add.email', {
+      url: '/email',
+      data: {navbarInfo: {title: 'Email address', enableBack: true, enableNext: true}},
+      templateUrl: 'shared/users/form.html',
+      controller: 'additions.add.email.controller',
+    })
+
+    .state('additions.add.first_name', {
+      url: '/first_name',
+      data: {navbarInfo: {title: 'First Name', enableBack: true, enableNext: true}},
+      templateUrl: 'shared/users/form.html',
+      controller: 'additions.add.first_name.controller',
+    })
+
+    .state('additions.add.last_name', {
+      url: '/last_name',
+      data: {navbarInfo: {title: 'Last Name', enableBack: true, enableNext: true}},
+      templateUrl: 'shared/users/form.html',
+      controller: 'additions.add.last_name.controller',
+    })
+
+    .state('additions.add.success', {
+      url: '/success',
+      data: {navbarInfo: {title: 'Success', enableMenu: true}},
+      templateUrl: 'shared/users/notice.html',
+      controller: 'additions.add.success.controller',
+    })
+})
---
+++ src/app/additions/routes.js
@@ -0,0 +1,37 @@
+'use strict';
+
+angular.module('idlecars')
+.config(function ($stateProvider) {
+
+  $stateProvider
+
+    .state('additions', {
+      data: {requireAuth: true},
+      url: '/',
+      views: {
+        'navbar': {
+          templateUrl: 'components/navbar/navbar_main.html',
+          controller: 'navbarMain.controller',
+        },
+        'content': {
+          templateUrl: 'app/additions/additions.html',
+          controller: 'additions.controller',
+        },
+      },
+    })
+
+    .state('medallion', {
+      url: '/medallion',
+      data: {navbarInfo: {title: 'Medallion', enableBack: true, enableNext: true}},
+      views: {
+        'navbar': {
+          templateUrl: 'components/navbar/navbar_main.html',
+          controller: 'navbarMain.controller',
+        },
+        'content@': {
+          templateUrl: 'shared/users/form.html',
+          controller: 'medallion.controller',
+        },
+      },
+    })
+});
