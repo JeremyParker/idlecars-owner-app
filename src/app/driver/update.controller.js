81517e7ed0a341190ff07893b3a7e126d770d1ed
craigstar <craig@idlecars.com>
2016-04-20 11:01:23 -0400

    owner can upload driver docs

--- src/app/driver/update.controller.js
+++ src/app/driver/update.controller.js
@@ -1,18 +1,23 @@
 'use strict';
 
 angular.module('idlecars')
-.controller('driver.update.proofaddress.controller', function ($scope, $state, HistoryService, CarService) {
+.controller('driver.update.proofaddress.controller', function ($scope, $state, $stateParams, HistoryService, MyOwnerService, CarService) {
   $scope.fileUrl = '/assets/images/address_proof_image.png';
   $scope.uploadTitle = 'your Motor Vehicle Record (optional)';
   $scope.associateToUser = function (fileUrl) {
     var patchData = {};
-    patchData['address_proof_image'] = fileUrl;
-    // request to server to change driver's doc
-    return CarService.get();
+    patchData['upload_driver_doc'] = {
+      'driver_id': $stateParams.driverId,
+      'doc_name': 'address_proof_image',
+      'file_url': fileUrl,
+    }
+    return MyOwnerService.patch(patchData);
   }
 
   $scope.afterUpload = function () {
+    // car info has been updated in the backend, we need to clean our cache
     // either go back to car detail page or go to home page.
+    CarService.clearCache()
     HistoryService.goPreviousState();
   }
 
